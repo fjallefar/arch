8814d7a481d2ad4e68f5986fa044ab8c666d5d7a
fjallefar <fjallefar@gmail.com>
2024-08-26 03:51:12 +0200

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -31,18 +31,15 @@ read -p "Do you want to install Spectacle (screenshot tool)? (y/n): " install_sp
 read -p "Do you want to install Neofetch (system information tool)? (y/n): " install_neofetch
 read -p "Do you want to install Teamspeak3? (y/n): " install_teamspeak3
 read -p "Do you want to install Steam? (y/n): " install_steam
-
 read -p "Do you want to install EasyEffects and related plugins? (y/n): " install_easyeffects
 read -p "Do you want to install 32-bit Pipewire? (y/n): " install_pipewire
+read -p "Do you want to install Dutree (disk usage tree viewer)? (y/n): " install_dutree
+read -p "Do you want to install Timeshift (system backup tool)? (y/n): " install_timeshift
 
 # Function to check user's yes/no response
 function user_choice() {
     [[ "$1" == "y" || "$1" == "Y" ]]
 }
-
-# Calculate number of cores to use for makepkg
-TOTAL_CORES=$(nproc)
-CORES_FOR_MAKEPKG=$((TOTAL_CORES - 2))
 
 # Configure GRUB for NVIDIA DRM
 if user_choice "$configure_grub"; then
@@ -65,18 +62,11 @@ if user_choice "$configure_mkinitcpio"; then
   NVIDIA_MODULES="nvidia nvidia_modeset nvidia_uvm nvidia_drm"
   MODULES_LINE=$(grep "^MODULES=" /etc/mkinitcpio.conf)
 
-  # Append NVIDIA modules if they are not already present
   if echo "$MODULES_LINE" | grep -qE "nvidia|nvidia_modeset|nvidia_uvm|nvidia_drm"; then
     echo "NVIDIA modules are already present in MODULES, skipping mkinitcpio update."
   else
-    echo "Appending NVIDIA modules to mkinitcpio.conf..."
-    # Extract existing modules and append NVIDIA modules
-    EXISTING_MODULES=$(sed -n 's/^MODULES=\(.*\)/\1/p' /etc/mkinitcpio.conf)
-    # Remove leading and trailing parentheses
-    EXISTING_MODULES=$(echo "$EXISTING_MODULES" | sed 's/^[()]//;s/[()]$//')
-    # Combine existing modules with NVIDIA modules
-    UPDATED_MODULES="($EXISTING_MODULES nvidia nvidia_modeset nvidia_uvm nvidia_drm)"
-    sed -i "s/^MODULES=.*/MODULES=$UPDATED_MODULES/" /etc/mkinitcpio.conf
+    echo "Adding NVIDIA modules to mkinitcpio.conf"
+    sed -i "/^MODULES=/ s/$(grep -oP '(?<=^MODULES=\()[^\)]*(?=\))')/$NVIDIA_MODULES/" /etc/mkinitcpio.conf
     echo "Rebuilding initramfs..."
     mkinitcpio -P
   fi
@@ -86,14 +76,8 @@ fi
 
 # Enable multicore support for makepkg
 if user_choice "$configure_makepkg"; then
-  echo "Configuring multicore support for makepkg to use ${CORES_FOR_MAKEPKG} cores..."
-  # Uncomment the line if it's commented and set the value
-  if grep -q '^#MAKEFLAGS=' /etc/makepkg.conf; then
-    sed -i "s/^#MAKEFLAGS=\".*\"/MAKEFLAGS=\"-j${CORES_FOR_MAKEPKG}\"/" /etc/makepkg.conf
-  else
-    # Ensure the line is present and correctly set
-    grep -q '^MAKEFLAGS=' /etc/makepkg.conf || echo "MAKEFLAGS=\"-j${CORES_FOR_MAKEPKG}\"" >> /etc/makepkg.conf
-  fi
+  echo "Configuring multicore support for makepkg..."
+  sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(($(nproc) - 2))"/' /etc/makepkg.conf
 else
   echo "Skipping multicore support configuration for makepkg."
 fi
@@ -117,7 +101,7 @@ if user_choice "$install_yay"; then
 
   # Install AUR packages
   sudo -u "$NON_ROOT_USER" bash << EOF
-yay -S --noconfirm $(if user_choice "$install_brave"; then echo "brave-bin"; fi) $(if user_choice "$install_discord"; then echo "discord"; fi) $(if user_choice "$install_spectacle"; then echo "spectacle"; fi) $(if user_choice "$install_neofetch"; then echo "neofetch"; fi) $(if user_choice "$install_teamspeak3"; then echo "teamspeak3"; fi) $(if user_choice "$install_steam"; then echo "steam"; fi) $(if user_choice "$install_easyeffects"; then echo "calf lsp-plugins easyeffects"; fi) $(if user_choice "$install_pipewire"; then echo "lib32-pipewire"; fi)
+yay -S --noconfirm $(if user_choice "$install_brave"; then echo "brave-bin"; fi) $(if user_choice "$install_discord"; then echo "discord"; fi) $(if user_choice "$install_spectacle"; then echo "spectacle"; fi) $(if user_choice "$install_neofetch"; then echo "neofetch"; fi) $(if user_choice "$install_teamspeak3"; then echo "teamspeak3"; fi) $(if user_choice "$install_steam"; then echo "steam"; fi) $(if user_choice "$install_easyeffects"; then echo "calf lsp-plugins easyeffects"; fi) $(if user_choice "$install_pipewire"; then echo "lib32-pipewire"; fi) $(if user_choice "$install_dutree"; then echo "dutree"; fi) $(if user_choice "$install_timeshift"; then echo "timeshift"; fi)
 EOF
 else
   echo "Skipping AUR package installations."
